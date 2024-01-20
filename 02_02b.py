4aed5b77afad6aec6b5f97286a51d4ebee60dc42
Paul T <74333593+King-tobe@users.noreply.github.com>
2024-01-20 03:23:11 +0000

    Palindrome

--- 02_02b.py
+++ 02_02b.py
@@ -1,5 +1,18 @@
+from collections import deque 
+#function to check the word if its Palindrome
+def palindrome_checker(word):
+    #creating a deque stack
+    d = deque(word)
+    #while len is greater than 1
+    while len(d) > 1:
+        # check the word from left to right if they're equal
+        if d.pop() != d.popleft():
+            return False
+    return True
 
 def main():
+    w = "age"
+    print(palindrome_checker(w))
     #add code here
     return
 
