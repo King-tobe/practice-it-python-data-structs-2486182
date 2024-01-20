56eab3e69670528aeef272dd26362eeb356704d2
Paul T <74333593+King-tobe@users.noreply.github.com>
2024-01-20 02:59:58 +0000

    deque food items

--- 02_01b.py
+++ 02_01b.py
@@ -1,7 +1,13 @@
-
+from collections import deque
 def main():
     #add code here
-    return
+    store = deque(maxlen=5)
+    store.append("STA001")
+    list = ["Rice","beans","plantain","eba"]
+    store.extend(list)
+    store.append("DES001")
+    print(store)
+    return store
 
 if __name__ == "__main__":
     main()
